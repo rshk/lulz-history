dfcba191aa975a3b2d56dd4400f336ac0f26fd83
Samuele Santi <redshadow@hackzine.org>
2013-09-29 18:07:56 +0200

    Better configuration for testing

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 cd "$( dirname "$0" )"
 set -v
 export LULZ_CONF=$PWD/lulz.cfg
-exec py.test --ignore=build --pep8 -v --cov=LulzHistory --cov-report=term-missing LulzHistory
+exec py.test --ignore=build --pep8 -v --cov=LulzHistory --cov-report=term-missing "$@" LulzHistory
